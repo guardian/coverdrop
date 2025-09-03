bff8456502efab8b3bb5d6a09957ea205e616328
Secure Collaboration <secure.collaboration@theguardian.com>
2025-09-03 16:24:38 +0100

    Project import generated by Copybara.
    GitOrigin-RevId: 7cc4ff1ff6a698284130f232b4bc80090a72acc2

--- ios/scripts/tidy_current_branch.sh
+++ ios/scripts/tidy_current_branch.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 set -e;
+
+if brew list swiftlint &>/dev/null; then
+  brew upgrade swiftlint
+else
+  brew install swiftlint
+fi
+
+if brew list swiftformat &>/dev/null; then
+  brew upgrade swiftformat
+else
+  brew install swiftformat
+fi
+
 echo "[ ] Tidying current branch."
 
 # Navigate to the ios directory
 cd "$(dirname "$0")/.."
 
-swiftlint --fix
+swiftlint --config ./reference/.swiftlint.yml --fix
 echo "[+] swiftlint finished."
 
 git diff --name-only main | grep "..swift" | xargs -I % swiftformat ../% --config .swiftformat
